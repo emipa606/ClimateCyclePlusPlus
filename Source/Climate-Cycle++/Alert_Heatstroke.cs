45cf6e025349a31d356467405739a7f7a72c34b3
Mlie <emipa606@users.noreply.github.com>
2020-03-16 14:30:03 +0100

    First publish

---
+++ Source/Climate-Cycle++/Alert_Heatstroke.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ClimateCyclePlusPlus
+{
+    //"Adapted" from RimWorld.Alert_Hypothermia
+    public class Alert_Heatstroke : Alert_Critical
+    {
+        private IEnumerable<Pawn> HeatstrokeDangerColonists
+        {
+            get
+            {
+                foreach (Pawn p in PawnsFinder.AllMaps_FreeColonistsSpawned)
+                {
+                    if (!p.SafeTemperatureRange().Includes(p.AmbientTemperature))
+                    {
+                        Hediff heatstroke = p.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Heatstroke, false);
+                        if (heatstroke != null && heatstroke.CurStageIndex >= 3)
+                        {
+                            yield return p;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Alert_Heatstroke() => this.defaultLabel = "AlertHeatstroke".Translate();
+
+        public override TaggedString GetExplanation()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (Pawn current in this.HeatstrokeDangerColonists)
+            {
+                stringBuilder.AppendLine("    " + current.NameShortColored);
+            }
+            return "AlertHeatstrokeDesc".Translate(stringBuilder.ToString());
+        }
+
+        public override AlertReport GetReport()
+        {
+            return AlertReport.CulpritsAre(this.HeatstrokeDangerColonists.ToList());
+        }
+    }
+}
